6d0ccc30c73b8f5e3ec5be3ee7caf2850e98ef71
Henry Brown <henry.jy.brown@gmail.com>
2024-09-24 16:59:37 -0400

    cleaned up some things in crawl.py and ignored the database

--- server/crawl.py
+++ server/crawl.py
@@ -1,13 +1,11 @@
 import os
 import json
 
-from twisted.internet import reactor, defer
 from fastapi import BackgroundTasks
 
 from multiprocessing import Process
 
-from scrapy.crawler import CrawlerRunner, CrawlerProcess
-from scrapy.utils.log import configure_logging
+from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from scrapy import Spider
 
@@ -28,24 +26,24 @@ from crud import (
 import schemas
 
 
-# This function runs the two scrapers sequentially (from the docs) https://docs.scrapy.org/en/latest/topics/practices.html
-def run_scraper_deprecated(db: Session, query: str, session: UUID):
-    # Getting project settings and configuring logger
+# This function creates the scraping process (its passed into multiprocess as the target in the function below it)
+def scrape(spider: Spider, query: str = None):
     settings = get_project_settings()
-    configure_logging(settings)
-    runner = CrawlerRunner(settings)
+    process = CrawlerProcess(settings)
+    process.crawl(spider, query=query)
+    process.start(
+        stop_after_crawl=True, install_signal_handlers=False
+    )  # the script will block here until all crawling jobs are finished
 
-    # Function to run crawlers sequentially
-    @defer.inlineCallbacks
-    def crawl():
-        yield runner.crawl(ProductsSpider, query)
-        yield runner.crawl(ReviewsSpider)
-        reactor.stop()
 
-    crawl()
-    reactor.run()  # script blocks here until the last crawl call is finished
+# This function runs the two scrapers sequentially (from the docs) https://docs.scrapy.org/en/latest/topics/practices.html
+def run_scraper(db: Session, query: str, session: UUID):
+    for spider in [ProductsSpider, ReviewsSpider]:
+        p = Process(target=scrape, args=(spider, query))
+        p.start()
+        p.join()  # blocks until process is completed
 
-    # opening outputted json files and creating the products then the reviews
+        # opening outputted json files and creating the products then the reviews
     with open("products.json") as f:
         products = json.load(f)
         batch_create_product(db, products=products, session_id=session.id)
@@ -59,37 +57,8 @@ def run_scraper_deprecated(db: Session, query: str, session: UUID):
 
     update_scraping_session(db, session.id)
 
-    return
 
-
-def scrape(spider: Spider, query: str = None):
-    settings = get_project_settings()
-    process = CrawlerProcess(settings)
-    process.crawl(spider, query=query)
-    process.start(
-        stop_after_crawl=True, install_signal_handlers=False
-    )  # the script will block here until all crawling jobs are finished
-
-
-# def run_products(query: str):
-#     settings = get_project_settings()
-#     process = CrawlerProcess(settings)
-#     process.crawl(ProductsSpider, query=query)
-#     process.start(
-#         stop_after_crawl=True, install_signal_handlers=False
-#     )  # the script will block here until all crawling jobs are finished
-
-
-def run_scraper(db: Session, query: str, session: UUID):
-    for spider in [ProductsSpider, ReviewsSpider]:
-        p = Process(target=scrape, args=(spider, query))
-        p.start()
-        p.join()  # blocks until process is completed
-    # scrape(ProductsSpider, query=query)
-    # scrape(ReviewsSpider, query=query)
-
-
-# scrape
+# scrape reviews, this is the controller which is called from the scrape route
 def scrape_reviews(
     db: Session, scrape: schemas.Scrape, background_tasks: BackgroundTasks
 ):
